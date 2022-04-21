ab1a2da136b8ecca09504a06db578c2205380fc2
Ben Partington <bdpartington@gmail.com>
2022-04-21 20:05:35 +0100

    Wrote test for 1 dollar and 5 dollar inputs

--- give_change.rb
+++ give_change.rb
@@ -1,3 +1,11 @@
 def give_change(amount)
-  [1, 0, 0, 0, 0, 0]
+  change_arr = [0,0,0,0,0,0]
+
+  if amount == 1
+    change_arr[0] = 1
+  elsif amount == 5
+    change_arr[1] = 1
+  end
+
+  change_arr
 end
--- spec/give_change_spec.rb
+++ spec/give_change_spec.rb
@@ -1,7 +1,11 @@
 require './give_change'
 
 describe 'give_change' do
-  it 'calculates change of single digit input' do
+  it 'calculates change array of 1 dollar' do
     expect(give_change(1)).to eq([1,0,0,0,0,0])
+  end
+
+  it 'calculates change array of 5 dollars' do
+    expect(give_change(5)).to eq([0,1,0,0,0,0])
   end
 end
