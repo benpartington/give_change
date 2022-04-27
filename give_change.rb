c80209de9e853458af4604f140aef8ddae40e511
Ben Partington <bdpartington@gmail.com>
2022-04-27 16:33:54 +0100

    Refactor to use reverse method

--- give_change.rb
+++ give_change.rb
@@ -1,19 +1,29 @@
 def give_change(amount)
   change_arr = [0,0,0,0,0,0]
 
-  if amount < 5
-    change_arr[0] = amount
-  elsif amount == 5
-    change_arr[1] = 1
-  elsif amount == 10
-    change_arr[2] = 1
-  elsif amount == 20
-    change_arr[3] = 1
-  elsif amount == 50
-    change_arr[4] = 1
-  elsif amount == 100
-    change_arr[5] = 1
+  amount_reversed = 0
+
+  while amount > 0 do
+    amount_reversed = amount_reversed * 10
+    amount_reversed = amount_reversed + (amount % 10)
+    amount = amount / 10 
   end
 
-  change_arr
+  p amount_reversed.digits
+
+  # if amount < 5
+  #   change_arr[0] = amount
+  # elsif amount == 5
+  #   change_arr[1] = 1
+  # elsif amount == 10
+  #   change_arr[2] = 1
+  # elsif amount == 20
+  #   change_arr[3] = 1
+  # elsif amount == 50
+  #   change_arr[4] = 1
+  # elsif amount == 100
+  #   change_arr[5] = 1
+  # end
+
+  # change_arr
 end
