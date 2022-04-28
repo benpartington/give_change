098e9224c06283fbc29d7ce019fbc7e894417173
Ben Partington <bdpartington@gmail.com>
2022-04-28 17:16:51 +0100

    Big refactor using new approach

--- give_change.rb
+++ give_change.rb
@@ -1,29 +1,15 @@
 def give_change(amount)
-  change_arr = [0,0,0,0,0,0]
-
-  amount_reversed = 0
-
-  while amount > 0 do
-    amount_reversed = amount_reversed * 10
-    amount_reversed = amount_reversed + (amount % 10)
-    amount = amount / 10 
-  end
-
-  p amount_reversed.digits
-
-  # if amount < 5
-  #   change_arr[0] = amount
-  # elsif amount == 5
-  #   change_arr[1] = 1
-  # elsif amount == 10
-  #   change_arr[2] = 1
-  # elsif amount == 20
-  #   change_arr[3] = 1
-  # elsif amount == 50
-  #   change_arr[4] = 1
-  # elsif amount == 100
-  #   change_arr[5] = 1
-  # end
-
-  # change_arr
+  array = []
+  array[5] = amount / 100
+  amount = amount % 100
+  array[4] = (amount) / 50
+  amount = amount % 50
+  array[3] = (amount) / 20
+  amount = amount % 20
+  array[2] = (amount) / 10
+  amount = amount % 10
+  array[1] = (amount) / 5
+  amount = amount % 5
+  array[0] = (amount) / 1
+  return array
 end
--- spec/give_change_spec.rb
+++ spec/give_change_spec.rb
@@ -1,35 +1,35 @@
 require './give_change'
 
 describe 'give_change' do
-  # it 'calculates change array of 1 dollar' do
-  #   expect(give_change(1)).to eq([1,0,0,0,0,0])
-  # end
+  it 'calculates change array of 1 dollar' do
+    expect(give_change(1)).to eq([1,0,0,0,0,0])
+  end
 
-  # it 'calculates change array of 5 dollars' do
-  #   expect(give_change(5)).to eq([0,1,0,0,0,0])
-  # end
+  it 'calculates change array of 5 dollars' do
+    expect(give_change(5)).to eq([0,1,0,0,0,0])
+  end
 
-  # it 'calculates change array of 10 dollars' do
-  #   expect(give_change(10)).to eq([0,0,1,0,0,0])
-  # end
+  it 'calculates change array of 10 dollars' do
+    expect(give_change(10)).to eq([0,0,1,0,0,0])
+  end
 
-  # it 'calculates change array of 20 dollars' do
-  #   expect(give_change(20)).to eq([0,0,0,1,0,0])
-  # end
+  it 'calculates change array of 20 dollars' do
+    expect(give_change(20)).to eq([0,0,0,1,0,0])
+  end
 
-  # it 'calculates change array of 50 dollars' do
-  #   expect(give_change(50)).to eq([0,0,0,0,1,0])
-  # end
+  it 'calculates change array of 50 dollars' do
+    expect(give_change(50)).to eq([0,0,0,0,1,0])
+  end
 
-  # it 'calculates change array of 100 dollars' do
-  #   expect(give_change(100)).to eq([0,0,0,0,0,1])
-  # end
+  it 'calculates change array of 100 dollars' do
+    expect(give_change(100)).to eq([0,0,0,0,0,1])
+  end
 
-  # it 'calculates change array of 2 dollars' do
-  #   expect(give_change(2)).to eq([2,0,0,0,0,0])
-  # end
+  it 'calculates change array of 2 dollars' do
+    expect(give_change(2)).to eq([2,0,0,0,0,0])
+  end
 
   it 'calculates change array of 6 dollars' do
-    expect(give_change(612)).to eq([1,1,0,0,0,0])
+    expect(give_change(6)).to eq([1,1,0,0,0,0])
   end
 end
